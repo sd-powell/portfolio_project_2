c166dafbcdff161f82d866384575539d3938fe69
sd-powell <kf210rhd@students.codeinstitute.net>
2025-02-18 10:56:19 +0000

    declare API URLs for quiz difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+//QUIZ API SETTINGS
+const easyQuiz =
+  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=easy&type=multiple";
+const mediumQuiz =
+  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=medium&type=multiple";
+const hardQuiz =
+  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=hard&type=multiple";
+
 // Declare const variables for site interactivity
