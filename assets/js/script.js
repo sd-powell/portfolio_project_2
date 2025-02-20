c92db8793a6a4ada66afb6f4aea9dce1e481bf2d
sd-powell <kf210rhd@students.codeinstitute.net>
2025-02-20 10:07:59 +0000

    fix API URLs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 //QUIZ API SETTINGS
 const easyQuiz =
-  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=easy&type=multiple";
+  "https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=multiple";
 const mediumQuiz =
-  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=medium&type=multiple";
+  "https://opentdb.com/api.php?amount=5&category=12&difficulty=medium&type=multiple";
 const hardQuiz =
-  "https://opentdb.com/api.php?command=request&amount=5&category=12&difficulty=hard&type=multiple";
+  "https://opentdb.com/api.php?amount=5&category=12&difficulty=hard&type=multiple";
 
 // Declare const variables for site interactivity
 // info_panel buttons
 const start_btn = document.getElementById("start_btn");
 const leaderboard_btn = document.getElementById("leaderboard_btn");
 const rules_btn = document.getElementById("rules_btn");
+
 // info_panel container
 const openRules = document.getElementById("rules_panel");
+
 // rules_panel buttons
 const exit_btn = document.getElementById("exit_btn");
 const rules_start_btn = document.getElementById("rules_start_btn");
